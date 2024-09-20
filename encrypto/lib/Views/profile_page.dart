9edc41a12f6a15b237bf4e279c6dd56feb12059c
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-09-20 16:58:52 +0530

    Profile Page base

--- encrypto/lib/Views/profile_page.dart
+++ encrypto/lib/Views/profile_page.dart
@@ -1,10 +1,68 @@
+import 'package:app/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: CyberpunkColors.oxfordBlue,
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 200,
+              decoration: const BoxDecoration(
+                  color: CyberpunkColors.fluorescentCyan,
+                  shape: BoxShape.circle),
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            Text(
+              'Dhruv Jain',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              "dhruvjainz",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [Text('500',style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.w600)), Text('Connextions',style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.w600))],
+                ),
+                const SizedBox(width: 30,),
+                Column(
+                  children: [Text('2019',style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.w600)), Text("Joined",style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.w600))],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
